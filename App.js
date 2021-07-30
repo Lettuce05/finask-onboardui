6906ad86478019a2397e06dc192c4471f32edef7
Anthony Romaine <anthony.romaine1@gmail.com>
2021-07-29 19:27:30 -0700

    initial commit

--- App.js
+++ App.js
@@ -1,20 +1,68 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, {useState} from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import AppLoading from 'expo-app-loading';
+import * as Font from 'expo-font';
+import { Asset } from 'expo-asset';
+import Onboard from './components/Onboard';
+import { Feather } from '@expo/vector-icons';
+
+
+function cacheImages(images) {
+  return images.map(image => {
+    if (typeof image === 'string') {
+      return Image.prefetch(image);
+    } else {
+      return Asset.fromModule(image).downloadAsync();
+    }
+  });
+}
+
+function cacheFonts(fonts) {
+  return fonts.map(font => Font.loadAsync(font));
+}
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  const [isReady, setIsReady] = useState(false);
+
+  const loadAssetsAsync = async () => {
+    const imageAssets = cacheImages([
+      require('./assets/images/onboard1.png'),
+      require('./assets/images/onboard2.png'),
+      require('./assets/images/onboard3.png'),
+    ]);
+
+    const fontAssets = cacheFonts([
+      Feather.font,
+      {
+        "Play-Regular": require("./assets/fonts/Play-Regular.ttf"),
+        "Poppins-Regular": require("./assets/fonts/Poppins-Regular.ttf"),
+        "Poppins-Bold": require("./assets/fonts/Poppins-Bold.ttf")
+      }
+    ]);
+
+    await Promise.all([...imageAssets, ...fontAssets]);
+  }
+
+  if (!isReady){
+    return <AppLoading 
+      startAsync={loadAssetsAsync}
+      onFinish={() => setIsReady(true)}
+      onError={console.warn}
+    />;
+  } else {
+    return (
+      <View style={styles.container}>
+        <Onboard />
+      </View>
+    );
+  }
+  
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#000000',
     alignItems: 'center',
     justifyContent: 'center',
   },
---
+++ components/Onboard.js
@@ -0,0 +1,120 @@
+import React from 'react';
+import { Feather } from '@expo/vector-icons';
+import { StyleSheet, View, Text, Image } from 'react-native';
+import AppIntroSlider from 'react-native-app-intro-slider';
+
+const slides = [
+    {
+        key: "1",
+        title: "SET YOUR FINANCIAL GOALS",
+        text: "Your goals will help us to formulate the right recommendations for success",
+        image: require("../assets/images/onboard1.png"),
+        backgroundColor: "#000000"
+    },
+    {
+        key: "2",
+        title: "FOLLOW OUR TIPS AND TRICKS",
+        text: "We help our users to make the right financial decisions",
+        image: require("../assets/images/onboard2.png"),
+        backgroundColor: "#000000"
+    },
+    {
+        key: "3",
+        title: "ENJOY FINANCIAL SUCCESS",
+        text: "You can track your progress and achievements in a special section",
+        image: require("../assets/images/onboard3.png"),
+        backgroundColor: "#000000"
+    },
+]
+
+const Onboard = () => {
+
+    const renderItem = ({item}) => {
+        return (
+            <View style={styles.slide}>
+                {item.image && <Image style={styles.image} source={item.image} />}
+                <Text style={styles.title}>{item.title}</Text>
+                <Text style={styles.text}>{item.text}</Text>
+            </View>
+        )
+    }
+
+    const keyExtractor = (item) => item.key;
+
+    const renderNextButton = () => {
+        return (
+            <View style={styles.buttonCircleWrapper}>
+                <View style={styles.buttonCircle}>
+                    <Feather name="arrow-right" color="black" size={20} />
+                </View>
+            </View>
+        )
+    }
+
+    const renderDoneButton = () => {
+        return (
+            <View style={styles.buttonCircleWrapper}>
+                <View style={styles.buttonCircle}>
+                    <Feather name="arrow-right" color="black" size={20} />
+                </View>
+            </View>
+        )
+    }
+
+    return (
+        <AppIntroSlider
+            data={slides}
+            keyExtractor={keyExtractor} 
+            renderItem={renderItem} 
+            renderNextButton={renderNextButton}    
+            renderDoneButton={renderDoneButton}    
+        />
+    )
+}
+
+const styles = StyleSheet.create({
+    slide: {
+        flex: 1,
+        alignItems: "center",
+        justifyContent: "center",
+        backgroundColor: '#000000'
+    },
+    title: {
+        color: "#fff",
+        textAlign: "center",
+        width: 140,
+        height: 52,
+        fontFamily: "Poppins-Bold",
+        fontSize: 14,
+    },
+    text: {
+        color: "#fff",
+        textAlign: "center",
+        width: 270,
+        height: 54,
+        fontFamily: "Poppins-Regular",
+        fontSize: 14,
+        marginBottom: 150
+    },
+    image: {
+        marginVertical: 30
+    },
+    buttonCircle: {
+        width: 33,
+        height: 33,
+        backgroundColor: '#FFE4DA',
+        borderRadius: 20,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    buttonCircleWrapper: {
+        width: 50,
+        height: 50,
+        backgroundColor: 'rgba(255, 127, 80, .20)',
+        borderRadius: 30,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+})
+
+export default Onboard;
